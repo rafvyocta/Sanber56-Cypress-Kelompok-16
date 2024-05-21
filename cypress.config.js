3ac5463116e50f0b72e3f3edfd6173f507d522fa
unknown <muhamadreza.2020@student.uny.ac.id>
2024-05-21 16:08:18 +0700

    file login test baru

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  }, env: {
+    username: 'kelompok16@sanber.com', 
+    password: 'Sanbercode56'  
   },
 });
